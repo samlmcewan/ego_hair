1cc2c4f5da36c194c35da97464c2ba2eeb4599e7
samlmcewan <samlmcewan@gmail.com>
2022-07-17 17:49:00 +0100

    Add gallery slider to homepage

--- js/carousel.js
+++ js/carousel.js
@@ -1,7 +1,7 @@
 /**
 * Testimonials slider
 */
- new Swiper('.testimonials-slider', {
+ new Swiper('#reviews .testimonials-slider', {
     speed: 600,
     loop: true,
     autoplay: {
@@ -9,10 +9,14 @@
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   type: 'bullets',
+    //   clickable: true
+    // },
+    navigation: {
+      nextEl: '.swiper-button-next-review',
+      prevEl: '.swiper-button-prev-review'
     },
     breakpoints: {
       320: {
@@ -27,3 +31,30 @@
     }
   });
 
+/**
+* Gallery slider
+*/
+new Swiper('#gallery-slider .testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  navigation: {
+    nextEl: '.swiper-button-next-unique',
+    prevEl: '.swiper-button-prev-unique'
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
